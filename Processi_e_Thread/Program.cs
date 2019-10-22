2fa89ee6edc760f8390599f6efba6a0bcff6851e
studenti <studenti@rosselli.loc>
2019-10-22 09:04:54 +0200

    Aggiungere i file di progetto.

---
+++ Processi_e_Thread/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processi_e_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
